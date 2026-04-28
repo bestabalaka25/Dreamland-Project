167a06dab1f333cfb4e53a58bd374b49c1ab42f7
JxckWilsxn <jwilsxn7@gmail.com>
2026-04-28 03:12:19 +0100

    mapbox gl imported and map javascript added

---
+++ modules/mapbox.js
@@ -0,0 +1,79 @@
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
+
+const createMap = (data) => {	// Create a map instance
+	const map = new mapboxgl.Map({  // Add stuff to map
+		container: 'map', // container ID
+		style: 'mapbox://styles/mapbox/standard', // style URL
+		center: [0.472944, 44.42004], // starting position [lng, lat]
+		zoom: 5.0, // starting zoom
+	});
+
+	map.on('style.load', () => {
+    map.setConfigProperty('basemap', 'lightPreset', 'night');
+	});
+	
+	const markersById = {};
+	let markerClicked = false;
+
+	data.map(item => {
+		if (!item.stadiums) return;
+
+		markersById[item.id] = item.stadiums.map(stadium => {
+			const marker = new mapboxgl.Marker({
+				className: "marker",
+				color: "#050c1c",
+				scale: 1.5
+			})
+			.setLngLat(stadium.coordinates)
+			.addTo(map)
+
+			const infoPopup = new mapboxgl.Popup({
+				className: "infoPopup",
+				closeButton: false,
+				closeOnClick: false,
+				offset: [0, -55]
+			})
+			.setHTML(`
+				<div class="popup-content">
+					<h3>${stadium.name}</h3>
+				</div>
+			`)
+
+			marker.getElement().addEventListener("mouseenter", () => {
+				infoPopup
+				.setLngLat(stadium.coordinates)
+				.addTo(map)
+			})
+
+			marker.getElement().addEventListener("mouseleave", () => {
+				infoPopup
+				.remove()
+			})
+			
+			marker.getElement().addEventListener("click", () => {
+				if (markerClicked === false) {
+					map.flyTo({
+						center: stadium.coordinates,
+						pitch: 60,
+						zoom: 17
+					})
+					markerClicked = true; 
+				} else {
+					map.flyTo({
+						center: stadium.coordinates,
+						zoom: 5
+					}) 
+					markerClicked = false;
+				}
+			})
+
+			return marker;
+		});
+	});
+	return { map, markersById };
+};
+
+export { createMap };
