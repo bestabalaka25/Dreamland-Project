6df624a0c5b6af04f849d2a530dc945e8defb311
JxckWilsxn <jwilsxn7@gmail.com>
2026-04-28 02:57:17 +0100

    timeline html and api data implemented

---
+++ modules/timelineApi.js
@@ -0,0 +1,9 @@
+export const dataFetch = async () => {
+    try {
+        const response = await fetch("/timelineData.json");
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error.message)
+    };
+};
---
+++ modules/timelineMain.js
@@ -0,0 +1,33 @@
+import { init } from "./scroller.js";            
+import { dataFetch } from "./timelineApi.js";
+import { createMap } from "./mapbox.js";
+
+const main = document.querySelector("main");
+const scroll = main.querySelector("#scroll");
+const article = scroll.querySelector("article");
+
+const run = async () => {
+    const data = await dataFetch();
+
+    const { markersById, map } = createMap(data)
+
+    const container = article.querySelector(".tournament-years");
+    
+    container.innerHTML = data.map(item => `
+        <div class="year" data-step="${item.id}">
+            <div class="year-content">
+                <div class="year-content-container">
+                    <h1>${item.header}</h1>
+                    <div id="timeline-host-flag-container">${item.flagSrc}</div>
+                </div>
+                <p>${item.para}</p>
+                <img id="timeline-main-img" src="${item.imgSrc}" alt="${item.imgAlt}">
+            </div>
+        </div>    
+    `).join("");
+    
+    init(markersById, map);
+};
+run();
+
+export { run };    
